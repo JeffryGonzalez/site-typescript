bc728ac89518f66e5cd4032a2633ac365a0dbff7
Jeffry Gonzalez <jeff@hypertheory.com>
2025-06-13 18:27:56 -0400

    started

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, reference } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+references: defineCollection({loader: docsLoader(), schema: docsSchema() })
 };
